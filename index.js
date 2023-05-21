bb640efe81fbea8372d34b2207972214d7d7a4a2
Luandre Bernardi de Andrade <luandrebernardi@gmail.com>
2023-05-21 11:55:50 -0300

    [ADD] Adicionado URL para visualização.

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ const url = "https://fiesc.pandape.infojobs.com.br/?PageNumber=1&PageSize=999";
 const filtroLocalizacao = ['Florianópolis', 'São José', 'Palhoça', 'Biguaçu'];
 const filtroTitulo = ['TI', 'Programador', 'Desenvolvedor', 'JavaScript', 'Infraestrutura', 'Suporte']
 const result = new Crawling(url)
+const pathConstruct = (pathName) => url.split('/')[0] + '//' + url.split('/')[2] + pathName
+
 
 async function layoutFiesc() {
     let vagas = [];
@@ -13,11 +15,13 @@ async function layoutFiesc() {
             let detailLocation = $(this).find('div > .vacancy-detail > .vacancy-location:first').text();
             let detailVagas = $(this).find('div > .vacancy-detail > .mb-2').text().trim();
             let dataVagas = $(this).find('div > .vacancy-date').text().trim();
+            let link = $(this).attr('href');
             vagas.push({
                 'titulo': title,
                 'data': dataVagas,
                 'localizacao': detailLocation,
-                'descrição': detailVagas
+                'descrição': detailVagas,
+                'Link': pathConstruct(link)
             })
         });
     });
